30f92ac1cd59dfecea43bc48aaaa520cccb14ed7
mmedellinesg <mmedellinesg@gmail.com>
2024-05-06 13:47:41 -0400

    Modify getGPT4Response function

--- Get_GPT4_Response.py
+++ Get_GPT4_Response.py
@@ -2,6 +2,8 @@ import os.path
 import openai
 import configparser
 import time
+import json
+import re
 
 MAX_ERRORS = 5
 
@@ -11,36 +13,35 @@ def exceptionHandler(error):
     time.sleep(2)
 
 
-def getGPT4Response(prompt, config_dict):
-    openai.api_key = config_dict['openai_api_key']
+def getGPT4Response(prompt, prompt_prefix, config_dict):
+    client = openai.OpenAI(api_key=config_dict['openai_api_key'])
     try_count = 0
     while 1:
         if try_count < MAX_ERRORS:
             try:
-                response = openai.Completion.create(
-                    model = config_dict['gpt_model'],
-                    prompt=prompt,
-                    max_tokens=int(config_dict['max_tokens']),
-                    temperature=0,
-                    top_p=1,
-                    frequency_penalty=0,
-                    presence_penalty=0
-                )
+                response = client.chat.completions.create(
+                        model = config_dict['gpt_model'],
+                        messages = [
+                            {'role': 'system','content': prompt_prefix},
+                            {'role': 'user','content': prompt},
+                                    ],
+                        temperature = 0,
+                        max_tokens= int(config_dict['max_tokens']),
+                        top_p=1,
+                        frequency_penalty=0,
+                        presence_penalty=0
+                    )
                 time.sleep(1)
                 break
-            except openai.error.ServiceUnavailableError as e:
+            except openai.RateLimitError as e:
                 try_count += 1
                 exceptionHandler(e)
-            except openai.error.RateLimitError as e:
-                try_count += 1
-                exceptionHandler(e)
-            except openai.error.APIError as e:
+            except openai.APIError as e:
                 try_count += 1
                 exceptionHandler(e)
         else:
             raise Exception('Max errors reached. Exiting...')
     try:
-        # serialization to save the response
-        return response.to_dict_recursive()
+        return json.loads('{'+re.findall(r'\{(.*?)\}',response.choices[0].message.content.replace('\n',''))[0]+'}')
     except:
         return {}
